7a3c8622b23501851416a0dd7586d0b43a43c525
kasyima <mbelenzir@gmail.com>
2024-07-01 11:28:44 +0300

    First commit: Lifetimes

---
+++ target/debug/deps/strsplit-8a94b8eb391431ce.d
@@ -0,0 +1,5 @@
+/home/mbelenzla/Projects/strsplit/target/debug/deps/libstrsplit-8a94b8eb391431ce.rmeta: src/lib.rs
+
+/home/mbelenzla/Projects/strsplit/target/debug/deps/strsplit-8a94b8eb391431ce.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/strsplit-c201a00af5db1d64.d
@@ -0,0 +1,5 @@
+/home/mbelenzla/Projects/strsplit/target/debug/deps/libstrsplit-c201a00af5db1d64.rmeta: src/lib.rs
+
+/home/mbelenzla/Projects/strsplit/target/debug/deps/strsplit-c201a00af5db1d64.d: src/lib.rs
+
+src/lib.rs:
